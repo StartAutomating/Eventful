632c68fc634d9a05a21d6df008c9da205b73744f
James Brundage <@github.com>
2023-06-09 13:32:27 -0700

    Updating Eventful workflows

--- Eventful.GithubWorkflow.psdevops.ps1
+++ Eventful.GithubWorkflow.psdevops.ps1
@@ -2,9 +2,11 @@
 Import-BuildStep -Module Eventful
 New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildEventful -Environment @{
     NoCoverage = $true
-}|
-    Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
+} -OutputPath (
+    Join-Path $PSScriptRoot .github\workflows\TestAndPublish.yml
+)
 
-New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged |
-    Set-Content (Join-Path $PSScriptRoot .github\workflows\OnIssue.yml) -Encoding UTF8 -PassThru
+New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged -OutputPath (
+    Join-Path $PSScriptRoot .github\workflows\OnIssue.yml
+)
 
